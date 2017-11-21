76b3eb559b263df8762821d7708f0a9e1189eaef
mnspoc <33691580+mnspoc@users.noreply.github.com>
2017-11-21 21:13:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
+from twilio.rest import Client
 import os
 import sys
 import urllib
 import requests
 import json
-from twilio.rest
-import Client
 from flask import Flask, request, Response, make_response, jsonify, send_from_directory
 # from flask_cors import CORS, cross_origin
 from requests.auth import HTTPBasicAuth
