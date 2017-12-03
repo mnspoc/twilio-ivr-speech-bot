fe92995c79b1ea0341d39b7892a33ae7d70cf9de
mnspoc <33691580+mnspoc@users.noreply.github.com>
2017-12-03 23:06:17 +0000

    resolve indent error

--- main.py
+++ main.py
@@ -300,9 +300,9 @@ def add_to_sync(request_dict,apiIntent):
     request_dict['initial_question'] = ''
     request_dict['CallDate'] = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
     if (request_dict['SpeechResult'] is None):
-        request_dict['Intent'] = 'Caller said nothing'  
+         request_dict['Intent'] = 'Caller said nothing'  
      else:  
-    	request_dict['Intent'] = apiIntent
+         request_dict['Intent'] = apiIntent
     callback_data = json.dumps(request_dict)
     # print(callback_data)
 
