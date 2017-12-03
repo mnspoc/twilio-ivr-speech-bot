c5c08d1ecb74c4c49ba565d961fad8f671c349ed
mnspoc <33691580+mnspoc@users.noreply.github.com>
2017-12-03 23:54:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -299,9 +299,10 @@ def apiai_fulfillment():
 def add_to_sync(request_dict,apiIntent):
     request_dict['initial_question'] = ''
     request_dict['CallDate'] = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-    if (request_dict['SpeechResult'] is None):
-         request_dict['Intent'] = 'Caller said nothing'  
-    else:  
+    if ('SpeechResult' in request_dict):
+    	if (request_dict['SpeechResult'] is None):
+             request_dict['Intent'] = 'Caller said nothing'  
+    	else:  
          request_dict['Intent'] = apiIntent
     callback_data = json.dumps(request_dict)
     # print(callback_data)
